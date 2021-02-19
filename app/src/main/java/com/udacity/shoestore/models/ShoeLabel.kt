c9f55fd1777bb1bafee07bb8e7c69d752b87f182
paul77uk <paul77uk@gmail.com>
2021-02-19 09:22:45 +0000

    new commit

---
+++ app/src/main/java/com/udacity/shoestore/models/ShoeLabel.kt
@@ -0,0 +1,8 @@
+package com.udacity.shoestore.models
+
+data class ShoeLabel(
+    var shoeName: String = "",
+    var company: String = "",
+    var shoeSize: String = "",
+    var description: String = "",
+    )
